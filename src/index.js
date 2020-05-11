12589a6fc0f49155f5a279a472bd480b40bbd59e
solomon-lo <losolomon0@gmail.com>
2020-05-10 17:03:48 -0700

    my project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,140 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      turn_number: 0,
+      xIsNext: true,
+      current_player: null,
+      alert: "here are player alerts",
+    };
+  }
+
+
+  handleClick(i){
+
+  var center_checker;
+  center_checker = (this.state.xIsNext ? 'X' : 'O');
+  if (squares[4] == center_checker) {
+    this.setState({ alert:"checking center"});
+    var temp_content_i = squares[i];
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    if (calculateWinner(squares)) {
+      return;
+    }
+    else {
+      squares[i] = temp_content_i;
+      //didn't return because it didn't win. Must clear middle and increase turn number
+
+      //clears the middle
+      squares[4] = null;
+
+      //increases turn number
+      this.setState({ turn_number: this.state.turn_number + 1 });
+    }
+  }
+  else {
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+      turn_number: this.state.turn_number + 1,
+    });
+  }
+}
+
+
+  renderSquare(i) {
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
+  }
+
+  render() {
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+    let display_number = this.state.turn_number;
+
+
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="current_player">{this.state.current_player}</div>
+        <div className="alert">{this.state.alert}</div>
+        <div className="turn number">{display_number}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}{this.renderSquare(1)}{this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}{this.renderSquare(4)}{this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}{this.renderSquare(7)}{this.renderSquare(8)}
+        </div>
+      </div>
+    );
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board />
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* TODO */}</ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+// ========================================
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Game />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
